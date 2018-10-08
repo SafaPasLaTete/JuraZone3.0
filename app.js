b31be0b1e893b2870ecaadbedc9c9afa001d1807
Romane Dubois <romane.dubois@heig-vd.ch>
2018-10-08 15:58:17 +0200

    mais???

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 const mongoose = require('mongoose');
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/jurazonedb');
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/jurazonedb');
 
 
 // view engine setup
