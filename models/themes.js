90dde7ab2e0d439d4bb12f3e3974b59e71dc508e
Safa Shams <safa.shams@heig-vd.ch>
2018-10-15 15:41:51 +0200

    modelethemes

--- models/themes.js
+++ models/themes.js
@@ -2,7 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Define the schema for users
 const themeSchema = new Schema({
-    id: String
+    nom :{
+
+        type: String,
+        required : true,
+        unique: true,
+        validate : {
+
+            validator: validateThemeNameUniqueness,
+            message: 'Person {VALUE} already exists'
+
+        }
+
+    },
+
+    image : {
+
+        type : String,
+        required: false,
+
+    }
 });
+
+
+
+function validateThemeNameUniqueness(value, callback) {
+    const theme = this;
+    this.constructor.findOne().where('nom').equals(value).exec(function(err, existingThemes) {
+        callback(!err && (!existingThemes || existingThemes._id.equals(Themes._id)));
+    });
+}
 // Create the model from the schema and export it
 module.exports = mongoose.model('themes', themeSchema)
