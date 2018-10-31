9f0e6639339b59c66ddcdb163e85ed5b65d06986
Safa Shams <safa.shams@heig-vd.ch>
2018-10-31 17:51:20 +0100

    themeV5

--- routes/themes.js
+++ routes/themes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const Theme = require('../models/themes');
+const Message = require('../models/messages');
+const secretKey = process.env.SECRET_KEY || 'changeme';
 /* GET messages listing. */
 router.get('/', function(req, res, next) {
     Theme.find().sort('id').exec(function(err, themes) {
@@ -11,7 +13,61 @@ router.get('/', function(req, res, next) {
     });
 });
 
-router.post('/', function(req, res, next) {
+
+router.get('/:name',authenticate, function (req, res, next) {
+    Theme.find({ name: req.params.name }).sort('name').exec(function (err, theme) {
+        if (err) {
+            res.status(400).send('Erreur erreur');
+            return next(err)
+        }
+        res.send(theme)
+    })
+})
+
+
+
+
+router.get('/:id/messages', function (req, res, next) {
+
+    /*   Theme.findOne({ _id: req.params.id }).exec(function (err, theme) {
+        if (err) {
+            res.status(400).send('Erreur erreur');
+            return next(err)
+        }
+
+        Message.aggregate([
+            {
+                $match: {
+                    "theme" : theme._id
+                }
+            }
+        ], function (err, results) {
+            if (err) {
+                res.status(400).send('Erreur erreur');
+                return next(err)
+            }
+            res.send(results)
+        });
+
+
+    })*/
+    console.log(req.params.id);
+
+    Message.find({ theme: req.params.id }).exec(function (err, message) {
+        if (err) {
+            res.status(400).send('Erreur erreur');
+            return next(err)
+        }
+        res.send(message)
+
+    })
+})
+
+
+
+
+
+router.post('/',authenticate, function(req, res, next) {
 
     /* Accéder aux paramêtres dans le corps de la requète*/
 
@@ -22,7 +78,7 @@ router.post('/', function(req, res, next) {
 
     const theme = new Theme({
         name:temp.name,
-        photo:'',
+        photo:temp.photo,
         createdAt:new Date(),
     });
 
@@ -38,16 +94,71 @@ router.post('/', function(req, res, next) {
     });
 });
 
-router.delete('/:id', function (req, res, next) {
+router.delete('/:id',authenticate, function (req, res, next) {
 
 
         Theme.findByIdAndRemove(req.params.id).exec(function (err, theme) {
             if (err) {
                 return next(err)
             }
-            res.status(200).send({message: "le theme " + req.params.id + req.params.name + " a été supprimer"})
+            res.status(200).send({message: "le theme " + req.params.id + req.params.name + " a été supprimé"})
         })
     });
+
+
+router.get('/:id/nbrmessages', function (req, res, next) {
+
+    Theme.findOne({_id:req.params.id},(err,theme) => {
+        Message.aggregate([
+        {
+            $match: {
+                "theme": theme._id
+            }
+        },
+        {
+            $group: {
+                _id: '$theme',
+                messagesCount: {
+                    $sum: 1
+                }
+            }
+        }
+    ], function (err, results) {
+        if (err) {
+            return next(err);
+        }
+        res.send(results)
+    })
+})
+
+
+})
+
+function authenticate(req, res, next) {
+    // Ensure the header is present.
+    const authorization = req.get('Authorization');
+    if (!authorization) {
+        return res.status(401).send('Authorization header is missing');
+    }
+    // Check that the header has the correct format.
+    const match = authorization.match(/^Bearer (.+)$/);
+    if (!match) {
+        return res.status(401).send('Authorization header is not a bearer token');
+    }
+    // Extract and verify the JWT.
+    const token = match[1];
+    jwt.verify(token, secretKey, function(err, payload) {
+        if (err) {
+            return res.status(401).send('Your token is invalid or has expired');
+        } else {
+            req.currentUserId = payload.sub;
+            next(); // Pass the ID of the authenticated user to the next middleware.
+        }
+    });
+}
+
+
+
 
 module.exports = router;
 
