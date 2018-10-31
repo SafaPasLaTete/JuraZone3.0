2fe6d63016c56b6b9315bacaafa8549ae20d7174
Romane Dubois <romane.dubois@heig-vd.ch>
2018-10-31 18:21:16 +0100

    message fini sauf erreur

--- models/messages.js
+++ models/messages.js
@@ -27,7 +27,7 @@ const messageSchema = new Schema({
   author: {
     type: Schema.Types.ObjectId,
     ref: 'User',
-    default: null
+    default: null,
         validate: {
           isAsync: true,
           // Validate that the director is a valid ObjectId
@@ -38,7 +38,7 @@ const messageSchema = new Schema({
     theme: {
     type: Schema.Types.ObjectId,
     ref: 'Theme',
-    default: null
+    default: null,
     validate: {
           isAsync: true,
       // Validate that the director is a valid ObjectId
